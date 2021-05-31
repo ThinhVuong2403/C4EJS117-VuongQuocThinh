90962ce5f5afd7a141b0e395a444916e29dd0d7e
ThinhVuong2403 <83406246+ThinhVuong2403@users.noreply.github.com>
2021-05-31 21:33:25 +0700

    'addDay1'

---
+++ Day1/index.js
@@ -0,0 +1,59 @@
+// 1.1 Tên biến không hợp lệ trong JS:
+//     - Biến bắt đầu bằng số. VD: 2403thinh,...
+//     - Biến chứa các ký tự đặc biệt ngoại trừ "_" và "$"(Khoảng trắng cũng là ký tự đặc biệt). VD: th-anh2,...
+//     - Biến trùng với các từ khóa trong JS. VD: break, Char,...
+// 1.2 Trong JS, để kiểm tra kiểu dữ liệu ta có toán tử typeof. 
+//     VD:
+        // let x = 5;
+        // console.log(typeof x);
+        // =======================================================================================================
+
+
+// 2. 1-b, 2-a, 3-e, 4-c
+
+//=============================================================================================
+
+// 3. 
+    // let message = "Coding is great";
+    // message = "Coding might not be easy, but still great";
+    // console.log(message.toLowerCase());
+    // let studentCount = 0;
+    // studentCount = 11;
+    // studentCount += 1;
+    // console.log(studentCount);
+
+    //================================================================================================
+// 5. 
+    // alert("You look beautiful today");
+
+
+//=====================================================================================================
+
+//6. 
+    // let yourName = prompt("Hi there, your name please?");
+    // alert("Hi " + yourName);
+//======================================================================================================
+
+//7. 
+    // let firstName = prompt("Enter your first name");
+    // let lastName = prompt("Enter your last name");
+    // alert("Hi " + lastName + " " + firstName);
+
+//========================================================================================================
+
+//8. 
+    // let x = prompt("Enter side length of the square")
+    // let area = x * x;
+    // alert("The square area  is " + area);
+
+//===========================================================================================================
+//9. 
+    // let x = prompt("Enter the radius of the circle");
+    // let circleArea = x * x * 3.14;
+    // alert("The circle area is " + circleArea);
+
+    //=======================================================================================================
+//10.
+let x = prompt("Enter the temperature in Celclus");
+let tem = x * 9/5 + 32;
+alert(x + " (C) = " + tem + " (F)");
