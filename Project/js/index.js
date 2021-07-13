9874be6af7e4f49e20ec5335c54a73a4cbe8e819
ThinhVuong2403 <vuongquocthinh24031999@gmail.com>
2021-07-13 18:59:40 +0700

    'addsua'

--- Project/js/index.js
+++ Project/js/index.js
@@ -1,12 +1,12 @@
 
 let btnSignUpForm = document.getElementById("SignUp");
 btnSignUpForm.addEventListener('click',() => {
-    document.getElementById("login").style.display = "none";
+    document.getElementById("login-form").style.display = "none";
     document.getElementById("register").style.display = "block";
 });
 let btnSignInForm = document.getElementById("SignIn");
 btnSignInForm.addEventListener('click',() => {
-    document.getElementById("login").style.display = "block";
+    document.getElementById("login-form").style.display = "block";
     document.getElementById("register").style.display = "none";
 });
 
